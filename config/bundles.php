d31196d17106763adccc80f50e7149c7d5352ac2
Sranu <silvestroran@gmail.com>
2022-10-10 17:12:52 +0200

    installed security bundle

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
