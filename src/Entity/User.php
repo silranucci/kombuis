06e4ed8eb2002250ed94fbea27b86d4889fe4cd0
Sranu <silvestroran@gmail.com>
2022-10-12 19:38:10 +0200

    * added plainPassword Field in user * hashed fixture password * added PasswordCredential to User

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,11 +4,12 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user_`')]
-class User implements UserInterface
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -23,6 +24,11 @@ class User implements UserInterface
 
     #[ORM\Column(length: 50, nullable: true)]
     private ?string $firstName = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $password = null;
+
+    private ?string $plainPassword;
 
     public function getId(): ?int
     {
@@ -76,7 +82,7 @@ class User implements UserInterface
     public function eraseCredentials()
     {
         // If you store any temporary, sensitive data on the user, clear it here
-        // $this->plainPassword = null;
+        $this->plainPassword = null;
     }
 
     public function getFirstName(): ?string
@@ -90,4 +96,31 @@ class User implements UserInterface
 
         return $this;
     }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+
+    public function getPlainPassword(): string
+    {
+        return $this->plainPassword;
+    }
+
+
+    public function setPlainPassword(string $plainPassword): void
+    {
+        $this->plainPassword = $plainPassword;
+    }
+
+
+
 }
--- src/Factory/UserFactory.php
+++ src/Factory/UserFactory.php
@@ -4,6 +4,7 @@ namespace App\Factory;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Zenstruck\Foundry\RepositoryProxy;
 use Zenstruck\Foundry\ModelFactory;
 use Zenstruck\Foundry\Proxy;
@@ -29,11 +30,14 @@ use function Symfony\Component\DependencyInjection\Loader\Configurator\service_l
  */
 final class UserFactory extends ModelFactory
 {
-    public function __construct()
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
     {
         parent::__construct();
 
         // TODO inject services if required (https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services)
+        $this->passwordHasher = $passwordHasher;
     }
 
     protected function getDefaults(): array
@@ -42,6 +46,7 @@ final class UserFactory extends ModelFactory
             // TODO add your default values here (https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories)
             'email' => self::faker()->email(),
             'firstName' => self::faker()->firstName(),
+            'plainPassword' => 'tada',
         ];
     }
 
@@ -49,7 +54,14 @@ final class UserFactory extends ModelFactory
     {
         // see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
         return $this
-            // ->afterInstantiate(function(User $user): void {})
+            ->afterInstantiate(function(User $user): void
+            {
+               if ($user->getPlainPassword()){
+                   $user->setPassword(
+                       $this->passwordHasher->hashPassword($user, $user->getPlainPassword())
+                   );
+               }
+            })
         ;
     }
 
